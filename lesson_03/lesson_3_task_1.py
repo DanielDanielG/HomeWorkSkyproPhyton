a3691a28f148ef0752e82b8bb63fda3a3350472d
Daniel <Turok@Gmail.com>
2025-12-16 13:49:27 +0300

    .\lesson_03\

--- lesson_03/lesson_3_task_1.py
+++ lesson_03/lesson_3_task_1.py
@@ -1,6 +1,6 @@
 from user import User
 
 my_user = User("Даниил", "Голиков")
-print(my_user.get_name())
-print(my_user.get_last_name())
-print(my_user.get_last_name_get_last_name())
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
